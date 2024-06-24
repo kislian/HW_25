a8a148b3248cd5ed20c4bf71a499eb1eaf68c28d
Andrey <kisli_an@mail.ru>
2024-06-24 11:54:37 +0300

    HW_25

---
+++ src/test/java/pro/skype/shope/ShopeApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.skype.shope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
